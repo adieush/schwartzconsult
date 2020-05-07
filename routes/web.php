c91b75529966db2dfa6e6cb031c970fd63540d62
Alexey Shevchenko <adieush@gmail.com>
2020-05-07 12:14:30 +0300

    Roll back test

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ Route::get('/checkout/success', 'CheckoutController@success');
 Route::get('/checkout/fail', 'CheckoutController@fail');
 Route::get('/checkout/one-to-one', 'CheckoutController@oneToOne');
 Route::get('/checkout/agency-package', 'CheckoutController@agencyPackage');
-Route::get('/checkout/test', 'CheckoutController@test');
+//Route::get('/checkout/test', 'CheckoutController@test');
 Route::post('/checkout/response-to-server', 'CheckoutController@responseToServer');
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/logout', '\App\Http\Controllers\Auth\LoginController@logout');
